1967b53fcf08cffd33e759edaab524d19475e2b4
s16446 <s16446@pjwstk.edu.pl>
2020-04-09 20:03:35 +0200

    Zadanie 4.5 Parametry w SQL

--- CW3_WebApplication1/WebApplication1/DAL/MockDbService.cs
+++ CW3_WebApplication1/WebApplication1/DAL/MockDbService.cs
@@ -15,12 +15,14 @@ namespace WebApplication1.DAL
         static MockDbService() 
         {
             _students = new List<Student>();
+           
 
             using (var client = new SqlConnection(CONN_STR))
             using (var com = new SqlCommand())
             {
                 com.Connection = client;
                 com.CommandText = "select [IndexNumber], [FirstName], [LastName], [BirthDate] from dbo.Student;";
+
                 client.Open();
 
                 var dr = com.ExecuteReader();
@@ -43,8 +45,30 @@ namespace WebApplication1.DAL
 
         public IEnumerable<Student> GetStudent(string id) 
         {
-            List<Student> n = new List<Student>();
-            n.Add(_students.Find(x => x.IndexNumber.Equals(id)));
+            List <Student> n = new List<Student>();
+            string index_no = id;
+
+            using (var client = new SqlConnection(CONN_STR))
+            using (var com = new SqlCommand())
+            {
+                com.Connection = client;
+                com.CommandText = "select [IndexNumber], [FirstName], [LastName], [BirthDate] from dbo.Student WHERE IndexNumber = @index_no";
+                com.Parameters.AddWithValue("index_no", index_no);
+                client.Open();
+                
+                var dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    var st = new Student();
+                    st.IndexNumber = dr["IndexNumber"].ToString();
+                    st.FirstName = dr["FirstName"].ToString();
+                    st.LastName = dr["LastName"].ToString();
+                    st.BirthDate = DateTime.Parse(dr["BirthDate"].ToString()).ToShortDateString();
+                    n.Add(st);
+                }
+            }
+            
+            //n.Add(_students.Find(x => x.IndexNumber.Equals(id)));
             return n;
         }
 
@@ -76,7 +100,7 @@ namespace WebApplication1.DAL
                 "FROM dbo.Student st " +
                 "LEFT JOIN dbo.Enrollment e ON st.IdEnrollment = e.IdEnrollment " +
                 "LEFT JOIN dbo.Studies s ON e.IdStudy = s.IdStudy " +
-                "WHERE IndexNumber = '" + id + "' AND e.Semester = " + semester + ";";
+                "WHERE IndexNumber = '" + id + "' AND e.Semester = '" + semester + "'";
                 Console.WriteLine(com.CommandText);
                 client.Open();
                 wpisy = new List<Enrollment>();
